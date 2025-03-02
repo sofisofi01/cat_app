5e1dcc294673d35e2f761d90eb8d98490b4f36ee
Sophie <sofi.sofi.01@mail.ru>
2025-03-03 00:45:17 +0300

    add gitgnore

--- cat_proj/settings.py
+++ cat_proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d^+^xpxc&gs=5e!yj!j(w&f754&_sv4u^y93k!sss^_m7crohl"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
