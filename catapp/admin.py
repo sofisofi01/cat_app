ac18f89d7ec6ef362837477b202b3420cd3b8b5b
Sophie <sofi.sofi.01@mail.ru>
2025-03-02 23:05:39 +0300

    add models + create admin panel

--- catapp/admin.py
+++ catapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Prediction, Comment, ImageUpload
 
-# Register your models here.
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'tag', 'likes', 'created_at')
+    list_filter = ('tag',)
+    search_fields = ('text',)
+    ordering = ('-created_at',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'prediction', 'created_at')
+    search_fields = ('username', 'text')
+
+class ImageUploadAdmin(admin.ModelAdmin):
+    list_display = ('image', 'uploaded_at')
+
+admin.site.register(Prediction, PredictionAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(ImageUpload, ImageUploadAdmin)
