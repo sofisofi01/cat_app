61fa2718e21c5040f0e478ae20e70adde81824f9
VioletKarasik <51052509+VioletKarasik@users.noreply.github.com>
2025-03-30 10:07:54 +0300

    Web task tests (#12)

--- catapp/admin.py
+++ catapp/admin.py
@@ -1,24 +1,77 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Prediction, Comment, ImageUpload
 
-from .models import Comment, ImageUpload, Prediction
-
-
+@admin.register(Prediction)
 class PredictionAdmin(admin.ModelAdmin):
-    list_display = ("text", "tag", "likes", "created_at")
-    list_filter = ("tag",)
-    search_fields = ("text",)
-    ordering = ("-created_at",)
+    list_display = ('id', 'text_preview', 'tag', 'likes', 'created_at', 'image_preview')
+    list_filter = ('tag', 'created_at')
+    search_fields = ('text', 'tag')
+    readonly_fields = ('image_preview', 'avatar_preview')
+    date_hierarchy = 'created_at'
+    list_per_page = 20
 
+    fieldsets = (
+        (None, {
+            'fields': ('text', 'tag', 'likes')
+        }),
+        ('Media', {
+            'fields': ('image', 'image_preview', 'avatar', 'avatar_preview')
+        }),
+    )
 
+    def text_preview(self, obj):
+        return obj.text[:50] + '...' if obj.text else '-'
+    text_preview.short_description = 'Text'
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="150" style="max-height: 100px;" />', obj.image.url)
+        return "-"
+    image_preview.short_description = 'Image'
+
+    def avatar_preview(self, obj):
+        if obj.avatar:
+            return format_html('<img src="{}" width="50" style="border-radius: 50%;" />', obj.avatar.url)
+        return "-"
+    avatar_preview.short_description = 'Avatar'
+
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("username", "prediction", "created_at")
-    search_fields = ("username", "text")
+    list_display = ('id', 'username', 'text_preview', 'prediction_link', 'created_at')
+    list_filter = ('created_at', 'prediction__tag')  # Фильтр по тегу предсказания
+    search_fields = ('username', 'text', 'prediction__text')
+    raw_id_fields = ('prediction',)
+    list_per_page = 30
+    date_hierarchy = 'created_at'
 
+    def text_preview(self, obj):
+        return obj.text[:100] + '...' if obj.text else '-'
+    text_preview.short_description = 'Comment'
 
+    def prediction_link(self, obj):
+        if obj.prediction:
+            return format_html('<a href="/admin/catapp/prediction/{}/change/">{}</a>', 
+                             obj.prediction.id, 
+                             obj.prediction.text[:50] + '...' if obj.prediction.text else f'Prediction #{obj.prediction.id}')
+        return "-"
+    prediction_link.short_description = 'Prediction'
+
+@admin.register(ImageUpload)
 class ImageUploadAdmin(admin.ModelAdmin):
-    list_display = ("image", "uploaded_at")
+    list_display = ('id', 'name_preview', 'tag', 'uploaded_at', 'image_preview')
+    list_filter = ('tag', 'uploaded_at')  # Фильтры по тегу и дате
+    search_fields = ('name', 'tag')
+    readonly_fields = ('image_preview',)
+    list_per_page = 20
+    date_hierarchy = 'uploaded_at'
 
+    def name_preview(self, obj):
+        return obj.name[:50] + '...' if obj.name else '-'
+    name_preview.short_description = 'Name'
 
-admin.site.register(Prediction, PredictionAdmin)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(ImageUpload, ImageUploadAdmin)
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="200" style="max-height: 150px;" />', obj.image.url)
+        return "-"
+    image_preview.short_description = 'Image'
--- catapp/tests.py
+++ catapp/tests.py
@@ -1,3 +1,97 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.test import TestCase, override_settings
+from rest_framework.test import APITestCase
+from .models import Prediction, Comment, ImageUpload, PredictionTag
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+class PredictionModelTest(TestCase):
+    def test_prediction_creation(self):
+        """Тестирование создания предсказания со всеми полями"""
+        prediction = Prediction.objects.create(
+            text="Test prediction",
+            tag=PredictionTag.PHILOSOPHICAL,
+            likes=5
+        )
+        self.assertEqual(prediction.text, "Test prediction")
+        self.assertEqual(prediction.tag, PredictionTag.PHILOSOPHICAL)
+        self.assertEqual(prediction.likes, 5)
+        self.assertIsNotNone(prediction.created_at)
+        
+    def test_all_tags_work(self):
+        """Проверка всех возможных тегов"""
+        for tag in PredictionTag:
+            with self.subTest(tag=tag):
+                prediction = Prediction.objects.create(tag=tag)
+                self.assertEqual(prediction.tag, tag)
+    
+    def test_image_upload(self):
+        """Тестирование загрузки изображения"""
+        test_image = SimpleUploadedFile("test.jpg", b"file_content", "image/jpeg")
+        prediction = Prediction.objects.create(image=test_image)
+        self.assertTrue(prediction.image.name.startswith('predictions/'))
+        
+    def test_ordering(self):
+        """Проверка сортировки по умолчанию"""
+        p1 = Prediction.objects.create(text="First")
+        p2 = Prediction.objects.create(text="Second")
+        self.assertEqual(
+            list(Prediction.objects.order_by('-created_at')), 
+            [p2, p1]
+        )
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        self.prediction = Prediction.objects.create(text="Test")
+        
+    def test_comment_creation(self):
+        """Тестирование создания комментария"""
+        comment = Comment.objects.create(
+            prediction=self.prediction,
+            username="user",
+            text="Test comment"
+        )
+        self.assertEqual(comment.prediction, self.prediction)
+        self.assertEqual(comment.username, "user")
+        self.assertEqual(comment.text, "Test comment")
+        self.assertIsNotNone(comment.created_at)
+        
+    def test_comment_ordering(self):
+        """Проверка сортировки комментариев"""
+        c1 = Comment.objects.create(prediction=self.prediction, username="1", text="1")
+        c2 = Comment.objects.create(prediction=self.prediction, username="2", text="2")
+        self.assertEqual(
+            list(Comment.objects.order_by('-created_at')), 
+            [c2, c1]
+        )
+        
+    def test_comment_required_fields(self):
+        """Проверка обязательных полей"""
+        with self.assertRaises(IntegrityError):
+            Comment.objects.create(prediction=None)
+
+class ImageUploadModelTest(TestCase):
+    def test_image_creation(self):
+        """Тестирование загрузки изображения"""
+        test_file = SimpleUploadedFile("test.jpg", b"content", "image/jpeg")
+        img = ImageUpload.objects.create(
+            name="Test",
+            image=test_file,
+            tag=PredictionTag.FUNNY
+        )
+        self.assertEqual(img.name, "Test")
+        self.assertEqual(img.tag, PredictionTag.FUNNY)
+        self.assertTrue(img.image.name.startswith('uploads/'))
+
+@override_settings(APPEND_SLASH=False)
+class PredictionAPITest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.prediction = Prediction.objects.create(text="API Test")
+        cls.comment = Comment.objects.create(
+            prediction=cls.prediction,
+            username="api_user",
+            text="API comment"
+        )
+        cls.image = ImageUpload.objects.create(name="API Image")
+        
